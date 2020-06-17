4f2ba1fbffc960f2f649b5cebda88dfa9db5e9a8
IsraP <IsraP@users.noreply.github.com>
2020-06-17 16:34:16 -0300

    Removendo debugs

--- js/logado.js
+++ js/logado.js
@@ -7,7 +7,6 @@ Bloco_Onload: {
         texto += `<h2 class="text-primaria fontelegal" id="nome"><strong>Olá ${nome}!</strong></h2>
                          <h4 class="text-primaria fontelegal"><br>Como podemos te ajudar hoje?</h4>`;
         let algumaCoisa = document.getElementsByClassName("nome");
-        console.log(algumaCoisa.length)
         for(i = 0; i < 2; i++){
             algumaCoisa[i].innerHTML = texto;
         }
